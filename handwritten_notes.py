466387383a2063b361b61037fc88529f71c4470f
renaninteraminense <61882456+renaninteraminense@users.noreply.github.com>
2021-05-17 02:28:04 -0300

    Add files via upload

---
+++ handwritten_notes.py
@@ -0,0 +1,12 @@
+import pywhatkit
+
+pywhatkit.text_to_handwriting("""Sinta o prazer de viver,
+Sinta a felicidade dentro de você,
+A esperança existe, busque, acredite.
+A força está dentro de você.
+Quando se sentir sozinho olhe em direção a lua, lembre-se de que você nunca estará só.
+Você comanda os seus pensamentos, pense positivo, a energia vem de dentro de você.
+E nunca se esqueça de que é preciso acreditar.
+Por isso, não deixe que o hoje seja apenas mais um dia comum na sua vida.
+Os seus sonhos podem sim se tornar verdade.
+Só é necessário você acreditar e tudo pode se tornar realidade.""", rgb=(0, 0, 255))
